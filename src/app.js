68e36bd373e53a78bbfc72720f9e4fed762a4dca
JulianCRA <admin@julianr.xyz>
2018-09-28 14:49:34 +0700

    Use color of the centroid pixel.

--- src/app.js
+++ src/app.js
@@ -24,10 +24,12 @@ let handleFileSelect = (evt) => {
 document.getElementById("file").addEventListener("change", handleFileSelect, false);
 document.getElementById("gobtn").addEventListener("click", go, false);
 document.getElementById("qvertices").addEventListener("input", setQ, false);
+//document.getElementById("qvertices").addEventListener("change", setQ, false);
 
 function go(){
 	p5instance.toDelaunay();
 }
+
 function setQ(){
 	p5instance.setVerticesAmount(this.value);
 }
@@ -46,10 +48,11 @@ let sketch = (p) => {
 		amount = 5000;
 		p.createCanvas(_WIDTH, _HEIGHT);
 		p.image(img, 0, 0);
+		img.loadPixels();
 	}
 
 	p.toDelaunay = () => {
-		img.loadPixels();
+		
 		points = [];
 		let xx, yy;
 		for(let i = 0; i < amount; i++){
@@ -58,12 +61,13 @@ let sketch = (p) => {
 			points.push([xx, yy]);
 		}
 		delaunay = Delaunator.from(points);
+		//console.log(delaunay.triangles);
 		p.drawTriangles();
 	}
 
 	p.drawTriangles = () => {
 		p.clear();
-		
+		p.noStroke();
 
 		function averageColor(c1, c2, c3){
 			return [(c1[0]+c2[0]+c3[0])/3, (c1[1]+c2[1]+c3[1])/3, (c1[2]+c2[2]+c3[2])/3, 255];
@@ -79,36 +83,21 @@ let sketch = (p) => {
 		img.loadPixels();
 		let tr;
 		let pix;
-		let c1 = [];
-		let c2 = [];
-		let c3 = [];
-
+		
 		function forEachTriangle(ppp, ddd) {
 			for (let t = 0; t < ddd.triangles.length / 3; t++) {
 				tr = pointsOfTriangle(ddd, t).map(pnt => ppp[pnt]);
 				
-				pix = (tr[0][1] * img.width + tr[0][0]) * 4;
-				c1 = [img.pixels[pix], img.pixels[pix+1], img.pixels[pix+2]];
+				let xx = Math.round((tr[0][0]+tr[1][0]+tr[2][0])/3);
+				let yy = Math.round((tr[0][1]+tr[1][1]+tr[2][1])/3);
+				pix = (yy * img.width + xx) * 4
+				p.fill(img.pixels[pix], img.pixels[pix+1], img.pixels[pix+2], 255);
 
-				pix = (tr[1][1] * img.width + tr[1][0]) * 4;
-				c2 = [img.pixels[pix], img.pixels[pix+1], img.pixels[pix+2]];
-
-				pix = (tr[2][1] * img.width + tr[2][0]) * 4;
-				c3 = [img.pixels[pix], img.pixels[pix+1], img.pixels[pix+2]];
-
-				p.fill(averageColor(c1, c2, c3));
-				p.stroke(averageColor(c1, c2, c3));
 				p.triangle(tr[0][0], tr[0][1], tr[1][0], tr[1][1], tr[2][0], tr[2][1]);
 			}
 		}
 		forEachTriangle(points, delaunay);
-		/*for (let e = 0; e < delaunay.triangles.length; e++) {
-			if (e > delaunay.halfedges[e]) {
-				const pp = points[delaunay.triangles[e]];
-				const qq = points[delaunay.triangles[nextHalfedge(e)]];
-				p.line(pp[0], pp[1], qq[0], qq[1]);
-			}
-		}*/
+
 	}
 
 	p.setVerticesAmount = (q) => {
